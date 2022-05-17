e3cf1881ee1a7db72d6a446545cba7f7145ec14f
valvegan <valentinavaccari@hotmail.com>
2022-05-17 14:42:03 +0100

    gifted chat error

--- components/Chat.js
+++ components/Chat.js
@@ -1,24 +1,49 @@
 import React from "react";
 import { View, Text, Button } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
 
-//chat component 
+//chat component
 export default class Chat extends React.Component {
+  constructor() {
+    super();
+    //state initialization
+    this.state = {
+      messages: [],
+    };
+  }
 
   //doesn't show up the name
   componentDidMount() {
     let { name } = this.props.route.params;
     this.props.navigation.setOptions({ title: name });
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: "Hello developer",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any",
+          },
+        },
+      ],
+    });
   }
   render() {
     let { bg } = this.props.route.params;
 
     return (
-      <View style={{ flex: 1, backgroundColor: bg, justifyContent: "center", alignItems: "center" }}>
-        <Button
-          title="go to start"
-          onPress={() => this.props.navigation.navigate("Start")}
+      
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
         />
-      </View>
+   
     );
   }
 }
